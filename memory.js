46164ef51ce613f6af2070d1fe9f3a2c80540785
John Clark <inindev@gmail.com>
2018-10-11 07:57:12 -0400

    apple iie memory layout

--- memory.js
+++ memory.js
@@ -16,35 +16,106 @@
 class Memory
 {
     constructor(rom_cd, rom_ef) {
-        this.rom_cd = rom_cd;
-        this.rom_ef = rom_ef;
+        this._rom_cd = rom_cd;
+        this._rom_ef = rom_ef;
 
-        this.main = new Uint8Array(0x10000);   // 64k
-        this.main_b2 = new Uint8Array(0x1000); //  4k (d000 bank 2)
+        this._main = new Uint8Array(0x10000);   // 64k
+        this._main_bb = new Uint8Array(0x1000); //  4k (dxxx bank2)
 
-        this.aux = new Uint8Array(0x10000);    // 64k
-        this.aux_b2 = new Uint8Array(0x1000);  //  4k (d000 bank 2)
+        this._aux = new Uint8Array(0x10000);    // 64k
+        this._aux_bb = new Uint8Array(0x1000);  //  4k (dxxx bank2)
 
-        this.read_hooks = [];
-        this.write_hooks = [];
+        this._bsr_read = false;
+        this._bsr_write = false;
+        this._bsr_bank2 = false;
+
+        this._aux_zp = false;
+        this._aux_read = false;
+        this._aux_write = false;
+
+        this._dms_80store = false;
+        this._dms_page2 = false;
+        this._dms_hires = false;
+
+        this._read_hooks = [];
+        this._write_hooks = [];
     }
+
+
+    get bsr_read() { return this._bsr_read; }
+    set bsr_read(val) { this._bsr_read = (val!=0); }
+    get bsr_write() { return this._bsr_write; }
+    set bsr_write(val) { this._bsr_write = (val!=0); }
+    get bsr_bank2() { return this._bsr_bank2; }
+    set bsr_bank2(val) { this._bsr_bank2 = (val!=0); }
+
+    get aux_zp() { return this._aux_zp; }
+    set aux_zp(val) { this._aux_zp = (val!=0); }
+    get aux_read() { return this._aux_read; }
+    set aux_read(val) { this._aux_read = (val!=0); }
+    get aux_write() { return this._aux_write; }
+    set aux_write(val) { this._aux_write = (val!=0); }
+
+    // tech ref p.28
+    get dms_80store() { return this._dms_80store; }
+    set dms_80store(val) { this._dms_80store = (val!=0); }
+    get dms_page2() { return this._dms_page2; }
+    set dms_page2(val) { this._dms_page2 = (val!=0); }
+    get dms_hires() { return this._dms_hires; }
+    set dms_hires(val) { this._dms_hires = (val!=0); }
+
 
     read(addr) {
         addr &= 0xffff;
 
-        for(let read_hook of this.read_hooks) {
+        for(let read_hook of this._read_hooks) {
             const res = read_hook(addr);
             if(res != undefined) return res & 0xff;
         }
 
-        // e000-ffff
-        if(addr > 0xdfff) return this.rom_ef[addr & 0x1fff];
-        // d000-dfff
-        if(addr > 0xcfff) return this.rom_cd[(addr & 0x0fff) | 0x1000];
-        // c000-cfff
-        if(addr > 0xbfff) return this.rom_cd[addr & 0x0fff];
+        // 0000-01ff
+        if(addr < 0x0200) {
+            return this._aux_zp ? this._aux[addr] : this._main[addr];
+        }
 
-        return this.main[addr & 0xffff];
+        if(this._dms_80store) {
+            // 0400-07ff
+            if((addr & 0xfc00) == 0x0400) {
+                return this._dms_page2 ? this._aux[addr] : this._main[addr];
+            }
+
+            // 2000-3fff
+            if(this._dms_hires && ((addr & 0xe000) == 0x2000)) {
+                return this._dms_page2 ? this._aux[addr] : this._main[addr];
+            }
+        }
+
+        // 0200-bfff
+        if(addr < 0xc000) {
+            return this._aux_read ? this._aux[addr] : this._main[addr];
+        }
+
+        // c000-cfff
+        if((addr & 0xf000) == 0xc000) {
+            return this._rom_cd[addr & 0x0fff];
+        }
+
+        // d000-ffff (filter above): bank switched ram
+        // ram read
+        if(this._bsr_read) {
+            // d000-dfff: bank 2 read
+            if(this._bsr_bank2 && ((addr & 0xf000) == 0xd000)) {
+                return this._aux_zp ? this._aux_bb[addr & 0x0fff] : this._main_bb[addr & 0x0fff];
+            }
+            // d000-dfff bank 1 & e000-ffff
+            return this._aux_zp ? this._aux[addr] : this._main[addr];
+        }
+
+        // rom read
+        // d000-dfff: read dxxx rom
+        if((addr & 0xf000) == 0xd000) return this._rom_cd[(addr & 0x0fff) | 0x1000];
+        // e000-ffff: read ef rom
+        return this._rom_ef[addr & 0x1fff];
     }
 
     // little-endian read
@@ -52,29 +123,91 @@ class Memory
         return this.read(addr) | this.read(addr+1)<<8;
     }
 
+
     write(addr, val) {
         addr &= 0xffff;
         val &= 0xff;
 
-        for(let write_hook of this.write_hooks) {
+        for(let write_hook of this._write_hooks) {
             const res = write_hook(addr, val);
             if(res != undefined) return;
         }
 
-        this.main[addr] = val;
+        // 0000-01ff
+        if(addr < 0x0200) {
+            if(this._aux_zp) {
+                this._aux[addr] = val;
+            } else {
+                this._main[addr] = val;
+            }
+            return;
+        }
+
+        if(this._dms_80store) {
+            // 0400-07ff
+            if((addr & 0xfc00) == 0x0400) {
+                if(this._dms_page2) {
+                    this._aux[addr] = val;
+                } else {
+                    this._main[addr] = val;
+                }
+                return;
+            }
+            // 2000-3fff
+            if(this._dms_hires && ((addr & 0xe000) == 0x2000)) {
+                if(this._dms_page2) {
+                    this._aux[addr] = val;
+                } else {
+                    this._main[addr] = val;
+                }
+                return;
+            }
+        }
+
+        // 0200-bfff
+        if(addr < 0xc000) {
+            if(this._aux_write) {
+                this._aux[addr] = val;
+            } else {
+                this._main[addr] = val;
+            }
+            return;
+        }
+
+        // c000-cfff || d000-ffff (discard writes to rom areas)
+        if(((addr & 0xf000) == 0xc000) || !this._bsr_write) return;
+
+        // d000-ffff (filter above): bank switched ram
+
+        // d000-dfff: bank 2 write
+        if(this._bsr_bank2 && ((addr & 0xf000) == 0xd000)) {
+            // write dxxx bank2 (aux a & b)
+            if(this._aux_zp) {
+                this._aux_bb[addr & 0x0fff] = val;
+            } else {
+                this._main_bb[addr & 0x0fff] = val;
+            }
+            return;
+        }
+        // d000-dfff bank 1 & e000-ffff
+        if(this._aux_zp) {
+            this._aux[addr] = val;
+        } else {
+            this._main[addr] = val;
+        }
     }
 
     // memory read callback hook
     //   function(addr)
     //   return undefined to contunue processing
     add_read_hook(callback) {
-        if(!this.read_hooks.includes(callback)) {
-            this.read_hooks.push(callback);
+        if(!this._read_hooks.includes(callback)) {
+            this._read_hooks.push(callback);
         }
     }
     remove_read_hook(callback) {
-        if(this.read_hooks.includes(callback)) {
-            this.read_hooks = this.read_hooks.filter(e => e !== callback);
+        if(this._read_hooks.includes(callback)) {
+            this._read_hooks = this._read_hooks.filter(e => e !== callback);
         }
     }
 
@@ -82,13 +215,13 @@ class Memory
     //   function(addr, val))
     //   return undefined to contunue processing
     add_write_hook(callback) {
-        if(!this.write_hooks.includes(callback)) {
-            this.write_hooks.push(callback);
+        if(!this._write_hooks.includes(callback)) {
+            this._write_hooks.push(callback);
         }
     }
     remove_write_hook(callback) {
-        if(this.write_hooks.includes(callback)) {
-            this.write_hooks = this.write_hooks.filter(e => e !== callback);
+        if(this._write_hooks.includes(callback)) {
+            this._write_hooks = this._write_hooks.filter(e => e !== callback);
         }
     }
 
@@ -102,10 +235,22 @@ class Memory
     }
 
     reset() {
-        this.main.fill(0);
-        this.main_b2.fill(0);
-        this.aux.fill(0);
-        this.aux_b2.fill(0);
+        this._main.fill(0);
+        this._main_b2.fill(0);
+        this._aux.fill(0);
+        this._aux_b2.fill(0);
+
+        this._bsr_read = false;
+        this._bsr_write = false;
+        this._bsr_bank2 = false;
+
+        this._aux_zp = false;
+        this._aux_read = false;
+        this._aux_write = false;
+
+        this._dms_80store = false;
+        this._dms_page2 = false;
+        this._dms_hires = false;
     }
 
     // export data range in hexdump format
